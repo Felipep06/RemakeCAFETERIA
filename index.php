4a85e09568ab37fb8976e66b4666551697112559
ArionaldoJr <arionaldojunior7@gmail.com>
2025-11-14 10:53:12 -0300

    Update index.php
    teste 2

--- index.php
+++ index.php
@@ -1,35 +1,31 @@
 <?php
-// --------------------------
+// ---------------------------------------------
 // PROCESSAMENTO DO FORMULÁRIO
-// --------------------------
-
+// ---------------------------------------------
 $carrinhoMensagem = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
     $produto = $_POST["produto"] ?? "Produto não identificado";
-
-    // opções comuns
-    $acucar = $_POST["acucar"] ?? "Não informado";
+    $acucar = $_POST["acucar"] ?? null;
     $leite = $_POST["tipo-leite"] ?? null;
-
-    // porções (muffin, cookies, pão de queijo)
     $porcao = $_POST["porcao"] ?? null;
 
-    // Montagem da mensagem
-    $carrinhoMensagem = "<div class='msg-carrinho'>";
+    $carrinhoMensagem = "<div style='
+        background:#333;
+        color:white;
+        padding:15px;
+        margin:20px;
+        border-radius:10px;
+        text-align:center;
+        border:2px solid #fff;
+        font-size:18px;
+    '>";
 
     $carrinhoMensagem .= "<strong>$produto</strong> foi adicionado ao carrinho!<br>";
 
-    if ($acucar !== "Não informado") {
-        $carrinhoMensagem .= "Açúcar: $acucar<br>";
-    }
-    if ($leite) {
-        $carrinhoMensagem .= "Leite: $leite<br>";
-    }
-    if ($porcao) {
-        $carrinhoMensagem .= "Porção escolhida: $porcao unidades<br>";
-    }
+    if ($acucar) $carrinhoMensagem .= "Açúcar: $acucar<br>";
+    if ($leite) $carrinhoMensagem .= "Leite: $leite<br>";
+    if ($porcao) $carrinhoMensagem .= "Porção escolhida: $porcao unidades<br>";
 
     $carrinhoMensagem .= "</div>";
 }
